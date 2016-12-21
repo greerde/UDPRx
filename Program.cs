dc2e07922496635a21c150d6367718c3b594ceff
Derek Greer <greerde@gmail.com>
2016-12-21 05:51:12 -0500

    Update class to match other PDW sim

--- Program.cs
+++ Program.cs
@@ -40,26 +40,23 @@ namespace ConsoleApplication
       PDW result = new PDW();
       using (MemoryStream m = new MemoryStream(data)) {
          using (BinaryReader reader = new BinaryReader(m)) {
-            result.Header = reader.ReadInt32();
-			result.dt = DateTime.FromBinary(reader.ReadInt64());
-			result.dtNanoSec = reader.ReadInt64();
+            		result.counter = reader.ReadInt32();
+			result.receiverID = reader.ReadInt32();
+			result.toa = reader.ReadInt64();
 			result.startFrequencyMHz = reader.ReadSingle();
 			result.stopFrequencyMHz = reader.ReadSingle();
-			result.receiverID = reader.ReadInt32();
 			result.pulseWidth = reader.ReadDouble();
 			result.snr = reader.ReadSingle();
 			result.phase = reader.ReadSingle();
 			result.phaseReferenceTime = reader.ReadSingle();
-			result.errorFlags = reader.ReadInt16();
-			result.modulationFlags = reader.ReadInt16();
+			result.errorFlags = reader.ReadInt32();
+			result.modulationFlags = reader.ReadInt32();
 			result.modulationParameters = reader.ReadSingle();
 			result.angleOfArrival = reader.ReadSingle();
 			result.riseTime = reader.ReadSingle();
 			result.fallTime = reader.ReadSingle();
 			result.pri = reader.ReadDouble();
 			
-			
-
 
          }
       }
@@ -70,31 +67,26 @@ namespace ConsoleApplication
 
     public class PDW
 	{
-		public int Header = 0; // using 32 bit int as header placeholder
-						//REPLACED: HeaderFlags recordHeader; //!< Record header
+	
+		public int counter = 0; // rolling counter 
+		public int receiverID = 1; //!< ID of the receiver that generated the PDW
+		
+		public long toa;
 
-		public DateTime dt;
-		public long dtNanoSec;
-		//REPLACED: HighResolutionTime toa; //!< UTC seconds with picosecond precision
-
-		public float startFrequencyMHz = 0; //!< Start frequency in MHz (TOA)
-		public float stopFrequencyMHz = 0; //!< Stop frequency in MHz (TOA + PW)
-		public int receiverID = 0; //!< ID of the receiver that generated the PDW
-		public double pulseWidth = 0; //!< Pulse width in seconds
-		public float pulseAmplitude = 0; //! Pulse amplitude in dBm (verify not dBW)
-		public float snr = 0; //!< Signal to noise ratio in dB
+		public float startFrequencyMHz = 1000; //!< Start frequency in MHz (TOA)
+		public float stopFrequencyMHz = 1000; //!< Stop frequency in MHz (TOA + PW)
+		public double pulseWidth = 0.0005; //!< Pulse width in seconds
+		public float pulseAmplitude = -5.4F; //! Pulse amplitude in dBm (verify not dBW)
+		public float snr = 10; //!< Signal to noise ratio in dB
 		public float phase = 0; //!< Phase in radians
 		public float phaseReferenceTime = 0; //!< Offset from TOA of phase measurement in seconds
-		public short errorFlags = 0;  // placeholder for error flags
-							   // REPLACED: ErrorFlags errorFlags; //!< Error conditions that occured during generation of this PDW
-		public short modulationFlags = 0;
-		//REPLACED: ModulationFlags modulationFlags; //!< Modulation types detected during generation of this PDW
+		public int errorFlags = 0;  // placeholder for error flags
+		public int modulationFlags = 0;
 		public float modulationParameters = 0; //!< in Hz or Hz/s as defined by EMD platinum
 		public float angleOfArrival = 0; //!< in radians
 		public float riseTime = 0; //!< Rise time in seconds
 		public float fallTime = 0; //!< Rise time in seconds
 		public double pri = 0; //!< Pulse repetition interval in seconds
-	
 
 
 	public byte[] Serialize()
@@ -103,12 +95,11 @@ namespace ConsoleApplication
 		{
 			using (BinaryWriter writer = new BinaryWriter(m))
 			{
-				writer.Write(Header);
-				writer.Write(dt.ToBinary());
-				writer.Write(dtNanoSec);
+				writer.Write(counter);
+				writer.Write(receiverID);
+				writer.Write(toa);
 				writer.Write(startFrequencyMHz);
 				writer.Write(stopFrequencyMHz);
-				writer.Write(receiverID);
 				writer.Write(pulseWidth);
 				writer.Write(pulseAmplitude);
 				writer.Write(snr);
